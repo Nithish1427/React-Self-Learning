76e0d8db53b0c954ed90cca111ca250224a51069
Nithish <nithishrajaduraip@gmail.com>
2025-11-20 13:38:00 +0530

    Creating First Component

---
+++ src/Course.jsx
@@ -0,0 +1,11 @@
+function Course() {
+    return (
+        <div>
+            <img src="" alt="" />
+            <h3>Mongo DB</h3>
+            <p>This is a Mongo DB Course.</p>
+        </div>
+    );
+}
+
+export default Course
