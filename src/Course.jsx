53998725a545cf8fc8c5fe49fc0979abe03620d1
Nithish <nithishrajaduraip@gmail.com>
2025-11-20 13:45:27 +0530

    Passing variables to HTML tags

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,8 +1,13 @@
+const course1 = "Mongo DB"
+
 function Course() {
     return (
         <div>
             <img src="" alt="" />
-            <h3>Mongo DB</h3>
+            {/* variable should be inside curly braces */}
+            <h3>{course1}</h3> 
+            {/* expressions can also be inside curly braces */}
+            {/* <h3>{1+4}</h3> */}
             <p>This is a Mongo DB Course.</p>
         </div>
     );
