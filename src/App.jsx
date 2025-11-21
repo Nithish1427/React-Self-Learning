c8940fc00ac6d2447381c8a805b55229afb015e2
Nithish <nithishrajaduraip@gmail.com>
2025-11-21 11:49:55 +0530

    Multiple Course Cards using Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Course from "./Course";
 import "./App.css";
+import MongoDB from "./assets/MongoDB.png"
+import ExpressJS from "./assets/Express.png"
+import ReactJS from "./assets/React.png"
+import NodeJS from "./assets/Node.png"
+import HTML5 from "./assets/HTML5.png"
+import CSS3 from "./assets/CSS3.png"
+import JavaScript from "./assets/JavaScript.png"
 
 function App() {
   return (
     <>
-      <Course />
+      <Course name="Mongo DB" price="199 rupees" image={MongoDB} />
+      <Course name="Express.js" price="119 rupees" image={ExpressJS} />
+      <Course name="React.js" price="149 rupees" image={ReactJS} />
+      <Course name="Node.js" price="119 rupees" image={NodeJS} />
+      <Course name="HTML5" price="99 rupees" image={HTML5} />
+      <Course name="CSS3" price="99 rupees" image={CSS3} />
+      <Course name="Java Script" price="199 rupees" image={JavaScript} />
     </>
   );
 }
--- src/Course.jsx
+++ src/Course.jsx
@@ -1,20 +1,13 @@
-import MongoDB from "./assets/MongoDB.png"
-
-const course1 = "Mongo DB"
-
-function Course() {
+function Course(props) {
 
     return (
         // internal css in Course.jsx inside Course() component
         <div className="card">
-            <div id="imgBlock">
-                <img src={MongoDB} alt="Mongo DB" />
+            <div className="imgBlock">
+                <img src={props.image} alt="Mongo DB" />
             </div>
-            {/* variable should be inside curly braces */}
-            <h3>{course1}</h3> 
-            {/* expressions can also be inside curly braces */}
-            {/* <h3>{1+4}</h3> */}
-            <p>This is a Mongo DB Course.</p>
+            <h3>{props.name}</h3> 
+            <p>{props.price}</p>
         </div>
     );
 }
