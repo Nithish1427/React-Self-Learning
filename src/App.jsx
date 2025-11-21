166aac63c878e36824ceb6c6d021e2ebe4ba2aff
Nithish <nithishrajaduraip@gmail.com>
2025-11-21 21:15:45 +0530

    Conditional Rendering 1

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ import JavaScript from "./assets/JavaScript.png"
 function App() {
   return (
     <>
-      <Course name="Mongo DB" price="199 rupees" image={MongoDB} rating="4.6" show={true} />
-      <Course name="Express.js" price="119 rupees" image={ExpressJS} rating="4.1" show={true} />
-      <Course name="React.js" price="149 rupees" image={ReactJS} rating="4.5" show={true} />
-      <Course name="Node.js" price="119 rupees" image={NodeJS} rating="4.4" show={true} />
-      <Course name="HTML5" price="99 rupees" image={HTML5} rating="4.3" show={true} />
-      <Course name="CSS3" price="99 rupees" image={CSS3} rating="4.2" show={true} />
-      <Course name="Java Script" price="199 rupees" image={JavaScript} rating="4.7" show={true} />
+      <Course name="Mongo DB" price={199} image={MongoDB} rating={4.6} show={true} />
+      <Course name="Express.js" price={119} image={ExpressJS} rating={4.1} show={true} />
+      <Course name="React.js" price={149} image={ReactJS} rating={4.5} show={true} />
+      <Course name="Node.js" price={119} image={NodeJS} rating={4.4} show={true} />
+      <Course name="HTML5" price={99} image={HTML5} rating={4.3} show={true} />
+      <Course name="CSS3" price={99} image={CSS3} rating={4.2} show={true} />
+      <Course name="Java Script" price={199} image={JavaScript} rating={4.7} show={true} />
     </>
   );
 }
--- src/Course.jsx
+++ src/Course.jsx
@@ -19,43 +19,9 @@ function Course({
       </div>
     );
   } else {
-    return <div className="card">Course Not Available</div>;
+    <div>Course Not Available</div>;
   }
 }
 
-// This always works
-// Does NOT depend on defaultProps
-// Works even if API returns undefined, null, or doesn’t include the field
 
-export default Course;
-
-// function Course(props) {
-
-//     return (
-//         <div className="card">
-//             <div className="imgBlock">
-//                 <img src={props.image} alt={props.name} />
-//             </div>
-//             <h3>{props.name}</h3>
-//             <p>{props.price}</p>
-//         </div>
-//     );
-// }
-
-// Course.defaultProps = {
-//     name : "Course",
-//     price : "Coming Soon",
-//     image : noImg
-// }
-
-// This works only when:
-// No parent prop is passed
-// NOT when the parent passes undefined
-// NOT when the parent passes an empty string
-// NOT when API returns { image: undefined }
-
-// So React “thinks” you did pass props — so defaults are ignored.
-
-// React no longer applies defaultProps on function components in Vite/ESM setups — especially when the prop is explicitly passed as undefined or missing from an API object.
-
-// export default Course
+export default Course
