56534888439b46e26be2d58bb7ec469fe1b2b56a
Nithish <nithishrajaduraip@gmail.com>
2025-11-26 11:54:14 +0530

    onClick() - Event Handling 3

--- src/Course.jsx
+++ src/Course.jsx
@@ -3,10 +3,11 @@ import PropTypes from "prop-types";
 
 function Course(props){
 
-  function BuyCourse(discount) {
+  function BuyCourse(discount, e) {
     const finalPrice = props.price - ( props.price * ( discount / 100 ));
     alert("Purchasing "+props.name+" Course at "+finalPrice+" rupees after a "+props.discount+" % discount from the actual price "+props.price+"?");
     console.log(props.name,"Course purchased at",finalPrice,"rupees after a",props.discount,"% discount from the actual price",props.price,"?");
+    console.log(e); // event object printed in console
   }
 
   return (
@@ -17,7 +18,9 @@ function Course(props){
         </div>
         <h3>{props.name}</h3>
         <p>{props.price}</p>
-        <button onClick={() => BuyCourse(props.discount)}>Purchase</button>
+        <button onClick={(event) => BuyCourse(props.discount, event)}>Purchase</button>
+        {/* can also be given in multiple line inside arrow function */}
+        {/* <button onClick={(event) => {BuyCourse(props.discount); console.log(event)}}>Purchase</button> */}
       </div>
     )
   );
