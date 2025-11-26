dc71611910208543db990dec1bedcc9f8fb360a1
Nithish <nithishrajaduraip@gmail.com>
2025-11-26 12:19:26 +0530

    Hooks - Introduction

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,13 +1,13 @@
-// import noImg from "./assets/ImageComingSoon.jpg";
 import PropTypes from "prop-types";
 
 function Course(props){
 
-  function BuyCourse(discount, e) {
+  let purchased = false;
+
+  function BuyCourse(discount) {
     const finalPrice = props.price - ( props.price * ( discount / 100 ));
     alert("Purchasing "+props.name+" Course at "+finalPrice+" rupees after a "+props.discount+" % discount from the actual price "+props.price+"?");
     console.log(props.name,"Course purchased at",finalPrice,"rupees after a",props.discount,"% discount from the actual price",props.price,"?");
-    console.log(e); // event object printed in console
   }
 
   return (
@@ -18,9 +18,8 @@ function Course(props){
         </div>
         <h3>{props.name}</h3>
         <p>{props.price}</p>
-        <button onClick={(event) => BuyCourse(props.discount, event)}>Purchase</button>
-        {/* can also be given in multiple line inside arrow function */}
-        {/* <button onClick={(event) => {BuyCourse(props.discount); console.log(event)}}>Purchase</button> */}
+        <button onClick={() => BuyCourse(props.discount)}>Purchase</button>
+        <p>{purchased ? "Already purchased" : "Get it now"}</p>
       </div>
     )
   );
