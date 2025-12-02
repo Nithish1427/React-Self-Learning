b6ff4c5d1fec403ff0ded2fe7149e4a0c6529110
Nithish <nithishrajaduraip@gmail.com>
2025-12-02 11:44:29 +0530

    Error Handling 1 - Fetching

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -2,27 +2,43 @@ import Course from "./Course";
 import { useState, useEffect } from "react";
 
 function CourseList() {
-
   const [courses, setCourses] = useState(null);
+
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     console.log("CourseList.jsx useEffect called");
 
-    fetch('http://localhost:3000/courses')
-    .then((response) => {console.log(response); // data from the API endpoint fetched as response and printed in console
-      return response.json(); // response into json format
-    }).then((data) => setCourses(data)) 
-    // here using setCourses inside useEffect is not a problem because of the empty dependency (since it is called only once)
-  }, []); 
+    fetch("http://localhost:3000/courses") // API Endpoint
+      .then((response) => {
+        if(!response.ok) {
+          throw Error("Couldn't retrieve data"); // Custom Error Message
+          // if the API Endpoint is incorrect, then this error message is displayed
+          // for example, "coursess" instead of "courses"
+        }
+        console.log(response); // data from the API endpoint fetched as response and printed in console
+        return response.json(); // response into json format
+      })
+      .then((data) => setCourses(data))
+      .catch((error) => {
+        // console.log(error); // prints error message with details
+        console.log(error.message); // prints just the "failed to fetch" message
+        setError(error.message);
+      });
+  }, []);
 
   function handleDelete(id) {
     setCourses(courses.filter((course) => course.id != id));
   }
 
   // Error - React tries to render the course components before fetching the data from API endpoint
-  // Error Handling 
-  if(!courses) {
-    return <></>
+  // Error Handling
+  if (!courses) {
+    return (
+      <>
+        <p>{error}</p>
+      </>
+    );
   }
 
   const coursesList = courses.map((course) => (
@@ -37,11 +53,7 @@ function CourseList() {
     />
   ));
 
-  return (
-    <>
-      {coursesList}
-    </>
-  );
+  return <>{coursesList}</>;
 }
 
 export default CourseList;
